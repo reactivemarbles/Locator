992092e2a141bd04f3d90c3288d5f9bd4d5eeb8d
Chris Pulman <chris.pulman@yahoo.com>
2021-08-30 00:49:21 +0100

    feature: Initial code (#1)
    Co-authored-by: Glenn <5834289+glennawatson@users.noreply.github.com>

---
+++ src/ReactiveMarbles.Locator/IEditServices.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+
+namespace ReactiveMarbles.Locator
+{
+    /// <summary>
+    /// I Edit Services.
+    /// </summary>
+    public interface IEditServices
+    {
+        /// <summary>
+        /// Adds the service.
+        /// </summary>
+        /// <param name="instanceFactory">The instance factory.</param>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <param name="contract">The contract.</param>
+        void AddService(Func<object?> instanceFactory, Type serviceType, string? contract = null);
+
+        /// <summary>
+        /// Removes the service of this type.
+        /// </summary>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <param name="contract">The contract.</param>
+        void RemoveService(Type serviceType, string? contract = null);
+
+        /// <summary>
+        /// Removes all services of this type.
+        /// </summary>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <param name="contract">The contract.</param>
+        void RemoveServices(Type serviceType, string? contract = null);
+    }
+}
